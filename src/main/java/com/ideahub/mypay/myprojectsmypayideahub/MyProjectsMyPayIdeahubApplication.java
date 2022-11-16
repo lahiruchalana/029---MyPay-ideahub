ccfba4fa58e525910d5a32d1a6028eb7059d956c
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-11-16 23:35:00 +0530

    first commit

---
+++ src/main/java/com/ideahub/mypay/myprojectsmypayideahub/MyProjectsMyPayIdeahubApplication.java
@@ -0,0 +1,13 @@
+package com.ideahub.mypay.myprojectsmypayideahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectsMyPayIdeahubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectsMyPayIdeahubApplication.class, args);
+    }
+
+}
